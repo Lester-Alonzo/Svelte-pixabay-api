c63b96cd6d5f8067517cdac5dde22777651a6993
Lester-Alonzo <lestergeo96@gmail.com>
2021-09-30 16:24:09 -0600

    configuraciones iniciales

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+import { onMount } from "svelte";
+const key = "23547014-a2604cbac9ac5f20f341a38e6";
+  const url =  ` https://pixabay.com/api/?key=${key}&q=${'anime'}&per_page=${'30'}&page=${'1'}`
+  
+  onMount(async () => {
+    const res = await fetch(url)
+    const imagen = res.json()
+    console.log(imagen);
+  })
+</script>
+<h1>Inicio</h1>
