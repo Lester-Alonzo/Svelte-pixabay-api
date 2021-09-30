c9d0eb2b61f27a0de9ce4c8b529a8d5766347bfb
Lester-Alonzo <lestergeo96@gmail.com>
2021-09-30 17:59:42 -0600

    prueba

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,21 @@
 <script>
 import { onMount } from "svelte";
+import Formulario from './Formulario.svelte'
 const key = "23547014-a2604cbac9ac5f20f341a38e6";
-  const url =  ` https://pixabay.com/api/?key=${key}&q=${'anime'}&per_page=${'30'}&page=${'1'}`
-  
-  onMount(async () => {
+let i
+let busqueda= ''
+  const url =  ` https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${'30'}&page=${'1'}`
+ const consulta = async () =>{ 
     const res = await fetch(url)
-    const imagen = res.json()
-    console.log(imagen);
-  })
+    const imagen = await res.json()    
+     return imagen
+ }
+ const nueva = e => {
+     busqueda = e.target.children[0].value
+ }
+ 
 </script>
-<h1>Inicio</h1>
+{#await consulta()}
+
+
+    <Formulario on:submit={nueva}/>
---
+++ src/Formulario.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+<form action="" on:submit|preventDefault>
+    <input type="text" name="busqueda"> 
+    <button type="submit">Buscar</button>       
+</form>
