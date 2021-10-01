185fd5a85f793e8183b67d9a555e80f98052f06b
Lester-Alonzo <lestergeo96@gmail.com>
2021-10-01 11:50:35 -0600

    estilos del header listo

--- src/Header.svelte
+++ src/Header.svelte
@@ -13,7 +13,11 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        border: 1px solid red;
         margin-bottom: 4em;
+        border-radius: 1rem;
+        box-shadow:1px 1px 9px rgba(0, 0, 0, 0.493);
+        background-color: rgb(43, 43, 43);
+        position:sticky;
+        top: 2px;
     }
 </style>
