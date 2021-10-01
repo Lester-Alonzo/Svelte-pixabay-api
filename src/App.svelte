0e114f1337b038595dc1b1890d42d8d60bb3845c
Lester-Alonzo <lestergeo96@gmail.com>
2021-10-01 11:03:59 -0600

    funciono

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,43 @@
 <script>
-import { onMount } from "svelte";
 import Formulario from './Formulario.svelte'
 import Imagen from './Imagen.svelte'
-let resul = []
-let ep = spera()
-let busqueda= ''
- async function spera(){ 
-const key = "23547014-a2604cbac9ac5f20f341a38e6";
-  const url =  ` https://pixabay.com/api/?key=${key}&q=${'anime'}&per_page=${'30'}&page=${'1'}`
-    const res = await fetch(url)
-    const imagen = await res.json()    
-    return imagen.hits
-   
- }
- const nueva = e => {
-     busqueda = e.target.children[0].value
-    consulta()
- }
+let espera = false
+ let promesa
+ const api =  async (dato) => {
+  const key = "23547014-a2604cbac9ac5f20f341a38e6";
+  const url = ` https://pixabay.com/api/?key=${key}&q=${dato}&per_page=${"30"}&page=${"1"}`;
+  const res = await fetch(url);
+  const imagens = await res.json();
+  return imagens.hits
+};
+
+const nueva = e =>{
+   promesa = api(e.target.children[0].value)
+   espera = true
+}
+ 
  
 </script>
-<Formulario on:submit={nueva}/>
+<Formulario on:submit={nueva} />
 
-{#await ep}
-  <!-- consulta is pending -->
-  <h1>Cargando</h1>
-{:then imagen}
-  <Imagen imagenes={imagen}/>
- {:catch error}
-  <!-- consulta was rejected -->
-  <h2>Error:{error}</h2>
-{/await}
+{#if espera}
+   <!-- content here -->
+   {#await promesa}
+     <!-- promise is pending -->
+     <h1>Cargando....</h1>
+   {:then valor}
+     <!-- promise was fulfilled -->
+    <Imagen imagenes={valor}/>
+   {:catch error}
+     <!-- promise was rejected -->
+     <h2>{error}</h2>
+   {/await}
+{:else}
+   <!-- else content here -->
+   <span>Realiza una busqueda</span>
+{/if}
+<style>
+  span,h1{
+    color: white;
+  }
+</style>
--- src/Formulario.svelte
+++ src/Formulario.svelte
@@ -1,5 +1,6 @@
 <script>
 
+
 </script>
 <form action="" on:submit|preventDefault>
     <input type="text" name="busqueda" placeholder="Ingresa una busqueda"> 
